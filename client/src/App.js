e59feef16e0f99bd439ae545106ca62bf5e473b5
admirerbrown <admirerbrown@gmail.com>
2023-08-31 18:29:12 +0000

    make hero section responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import HeroSection from './Components/Hero';
+import Hero from './Components/Hero';
 import About from './Components/About';
 
 function App() {
   return (
     <div className="App w-full">
-      <HeroSection></HeroSection>
-      <About></About>
+      <Hero></Hero>
+      {/* <About></About> */}
     </div>
   );
 }
