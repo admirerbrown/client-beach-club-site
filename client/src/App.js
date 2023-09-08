ea022d095d8014044df7f58e3fee752965e15c0e
admirerbrown <admirerbrown@gmail.com>
2023-09-08 12:37:56 +0000

    custom carousel almost complete but not responsive yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Hero from './Components/Hero';
 import About from './Components/About';
-// import AnimatedButton from './Components/newBtn';
+import Activities from './Components/Activities';
+import Carousel from './Components/carousel';
 
 function App() {
   return (
     <div className="App w-full">
       <Hero></Hero>
       <About></About>
-      {/* <AnimatedButton text="new man"></AnimatedButton> */}
+      <Activities></Activities>
+     <Carousel></Carousel>
     </div>
   );
 }
