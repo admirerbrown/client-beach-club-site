98d3113b47e06b38258ae67afbca6374f98d06f2
admirerbrown <admirerbrown@gmail.com>
2023-09-27 13:08:33 +0000

    menu responsive almost done

--- client/src/Components/Menu.jsx
+++ client/src/Components/Menu.jsx
@@ -36,22 +36,22 @@ const RenderMenu = () => {
 
   return (
     <div className=" pt-10">
-      <div className="font-Cormorant-Garamond  justify-center flex flex-col p-5 capitalize gap-7">
-        <h2 className=" text-2xl xl:text-3xl tracking-widest font-medium text-[#315779] xl:text-start xl:pl-5 2xl:pl-0">
+      <div className="font-Cormorant-Garamond  justify-center flex flex-col p-5 capitalize gap-7 w-full items-center">
+        <h2 className=" text-2xl md:text-lg md:font-semibold font-IBM-Plex-Sans tracking-[5px] uppercase font-medium text-[#315779] xl:pl-5 2xl:pl-0">
           check our menu
         </h2>
-        <h1 className="text-[29px] font-semibold md:text-4xl xl:text-5xl md:ml-5 text-[#315779] leading-10 mb-6 tracking-widest 2xl:ml-0">
+        <h1 className="text-[29px] font-semibold xl:font-bold xl:leading-[60px]  md:text-4xl xl:text-[43px] xl:w-[62%] xl:mx-0 xl:text-center md:mx-20 lg:mx-32 text-[#315779] leading-10 md:leading-[50px] mb-6 tracking-widest 2xl:ml-0">
           Savor the Coastal Delights, Bask in Sunshine, with a Wave of Beach
           Vibes.
         </h1>
       </div>
-      <div className="flex flex-wrap justify-center gap-5">
+      <div className="flex flex-wrap justify-center gap-5 mt-4 xl:gap-5 mb-4">
         {menu.map((item, index) => (
           <div
             key={index + 1}
             className="flex items-center flex-col gap-4 text-2xl text-[#315779]"
           >
-            <div className="rounded-full overflow-hidden border w-[9.5rem] h-[9.5rem] p-2.5 btn-breathe hover:border-[#A7D6FF]">
+            <div className="rounded-full overflow-hidden border w-[9.5rem] h-[9.5rem] xl:h-[10.5rem] xl:w-[10.5rem] md:h-[200px] md:w-[15rem] lg:w-[16rem] p-2.5 btn-breathe hover:border-[#A7D6FF]">
               <img
                 src={item.image}
                 alt=""
@@ -64,7 +64,7 @@ const RenderMenu = () => {
           </div>
         ))}
       </div>
-      <div className="mt-16">
+      <div className="mt-16 flex flex-col md:flex-row flex-wrap items-center justify-center px-5 md:px-0 gap-5 md:gap-8 xl:flex-nowrap xl:px-14">
         <MenuItems></MenuItems>
       </div>
     </div>
@@ -75,7 +75,7 @@ const MenuItems = () => {
   return (
     <>
       {Object.keys(foodList).map((category) => (
-        <div key={category} className="border m-5">
+        <div key={category} className=" border w-full md:w-[46%] xl:w-full">
           <div
             className={`${
               foodList[category][0].title === "Delicious Food"
@@ -83,7 +83,7 @@ const MenuItems = () => {
                 : "bg-[#E9F5FF]  text-[#315779]"
             } h-16 flex justify-center items-center border-b mb-3 `}
           >
-            <h2 className=" font-Cormorant-Garamond text-xl font-semibold">
+            <h2 className=" font-Cormorant-Garamond text-xl md:text-2xl xl:text-[26px] font-semibold">
               {foodList[category][0].title}
             </h2>
           </div>
@@ -92,12 +92,12 @@ const MenuItems = () => {
               key={index}
               className={`${
                 index === foodList[category].length - 2 ? "border-none" : ""
-              } h-24 border-b-2 justify-center flex  text-[#315779] flex-col mx-7 border-dotted font-Cormorant-Garamond`}
+              } h-24 border-b-2 border-[#E9F5FF] justify-center flex  text-[#315779] flex-col mx-7 border-dotted font-Cormorant-Garamond`}
             >
-              <h4 className="flex justify-between w-full text-xl font-semibold">
+              <h4 className="flex justify-between w-full text-xl md:text-[22px] font-semibold">
                 {item.name} <span>{item.price}</span>
               </h4>
-              <p className="text-left text-sm font-thin font-IBM-Plex-Sans">
+              <p className="text-left text-sm md:text-[15px] md:leading-6 font-thin font-IBM-Plex-Sans">
                 {item.description}
               </p>
             </div>
