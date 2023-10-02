b153883b1c39095ae5f27947a07fb5a0be3368c8
admirerbrown <admirerbrown@gmail.com>
2023-10-02 18:03:35 +0000

    testimonial section responsive

---
+++ client/src/Components/Testimonials.js
@@ -0,0 +1,44 @@
+// import { Parallax } from 'react-parallax'
+
+import React from 'react';
+
+const RenderTestimonials = () => {
+    return (
+        <div className='bg-fixed bg-cover bg-center bg-bg_image'>
+            <div className='mt-40 h-[500px] bg-[rgb(22,36,63,0.6)] text-white flex flex-col justify-end items-center relative'>
+                <div className='absolute bg-white h-[280px] border-2 border-red-700 my-5 flex items-center gap-5 p-5 justify-center bottom-0 transform translate-y-1/2 -translate-x-0'>
+                    <div className='flex justify-center '>
+                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
+                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                        </div>
+                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                        </div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
+                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                        </div>
+                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                        </div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
+                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                        </div>
+                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default RenderTestimonials;
+
+// TODO:MAKE TESTIMONIAL SECTION RESPONSIVE
