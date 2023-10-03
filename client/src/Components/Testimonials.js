e98b2e64b4697cdc51d71cfd6ad99c6717985120
admirerbrown <admirerbrown@gmail.com>
2023-10-03 21:55:11 +0000

    testimonial desktop almost done

--- client/src/Components/Testimonials.js
+++ client/src/Components/Testimonials.js
@@ -6,32 +6,49 @@ const RenderTestimonials = () => {
     return (
         <div className='bg-fixed bg-cover bg-center bg-bg_image'>
             <div className='mt-40 h-[500px] bg-[rgb(22,36,63,0.6)] text-white flex flex-col justify-end items-center relative'>
-                <div className='absolute bg-white h-[280px] border-2 border-red-700 my-5 flex items-center gap-5 p-5 justify-center bottom-0 transform translate-y-1/2 -translate-x-0'>
-                    <div className='flex justify-center '>
-                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
-                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                <div className='absolute bg-white w-[70%] border flex p-5 items-center justify-center bottom-0 transform translate-y-1/2 -translate-x-0'>
+                    <div className='flex flex-col justify-center  items-center text-[#315779] font-Cormorant-Garamond'>
+                        <div className="text-center  pb-7 bg-[#E9F5FF] shadow-sm">
+                            <p className='text-center  italic text-xl p-8  leading-8'>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
                         </div>
-                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
-                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
-                        </div>
-                    </div>
-                    <div className='flex justify-center'>
-                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
-                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
-                        </div>
-                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
-                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                        <div className='absolute bottom-0 flex flex-col items-center justify-center w-full'>
+                            <div className=" absolute flex shadow-md h-[105px] w-[105px] p-1.5 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                                <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                            </div>
+                            <div className='absolute  flex flex-col items-center justify-center mt-32 pt-8'>
+                                <p className=' relative  items-end text-[22px] leading-[28px] font-semibold'>Baldwin Fraley</p>
+                                <p className='relative  items-end text-xs tracking-[5px] uppercase'>Visitors</p>
+                            </div>
                         </div>
                     </div>
-                    <div className='flex justify-center'>
-                        <div className="text-center w-96 h-64 bg-[#E9F5FF] text-black">
-                            <p>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                    <div className='flex flex-col justify-center  items-center text-[#315779] font-Cormorant-Garamond'>
+                        <div className="text-center  pb-7 bg-[#E9F5FF] shadow-sm">
+                            <p className='text-center  italic text-xl p-8  leading-8'>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
                         </div>
-                        <div className="flex shadow-lg justify-end absolute h-24 w-24 p-1 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
-                            <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                        <div className='absolute bottom-0 flex flex-col items-center justify-center w-full'>
+                            <div className=" absolute flex shadow-md h-[105px] w-[105px] p-1.5 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                                <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                            </div>
+                            <div className='absolute  flex flex-col items-center justify-center mt-32 pt-8'>
+                                <p className=' relative  items-end text-[22px] leading-[28px] font-semibold'>Baldwin Fraley</p>
+                                <p className='relative  items-end text-xs tracking-[5px] uppercase'>Visitors</p>
+                            </div>
                         </div>
                     </div>
-
+                    <div className='flex flex-col justify-center  items-center text-[#315779] font-Cormorant-Garamond'>
+                        <div className="text-center pb-7 bg-[#E9F5FF] shadow-sm">
+                            <p className='text-center  italic text-xl p-8  leading-8'>Lorem ipsum Quibusdam eveniet eos rerum incididunt eu aperiam volup tatem ut non provident dolor dolorum ut et quod voluptatibus quas tempor reiciendis veniam provident.</p>
+                        </div>
+                        <div className='absolute bottom-0 flex flex-col items-center justify-center w-full'>
+                            <div className=" absolute flex shadow-md h-[105px] w-[105px] p-1.5 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800 -bottom-10">
+                                <img className='className="w-full h-full object-cover rounded-full' src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp" alt='' />
+                            </div>
+                            <div className='absolute  flex flex-col items-center justify-center mt-32 pt-8'>
+                                <p className=' relative  items-end text-[22px] leading-[28px] font-semibold'>Baldwin Fraley</p>
+                                <p className='relative  items-end text-xs tracking-[5px] uppercase'>Visitors</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -42,3 +59,4 @@ const RenderTestimonials = () => {
 export default RenderTestimonials;
 
 // TODO:MAKE TESTIMONIAL SECTION RESPONSIVE
+//TODO: FIX THE NAME TAG ASSOCIATED WITH THE IMAGE  
