0c5240bcc486c69ac33eb309d2bf2c0e1f9efb46
admirerbrown <admirerbrown@gmail.com>
2023-10-23 11:57:12 +0000

    navbar responsive toggling phase2

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -4,72 +4,9 @@
 import { MdMenu, MdOutlineClose, MdKeyboardArrowDown } from "react-icons/md";
 import { useState } from "react";
 
-// const NavBar = () => {
-//   return (
-//     <div className="">
-//       <div className="navbar bg-[rgb(212,212,216,.1)] h-24 shadow-sm border-b backdrop-blur-md border-[#96989B] border-solid">
-//         <div className="navbar-start">
-//           <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-//         </div>
-//         <div className="navbar-center hidden lg:flex">
-//           <ul className="menu menu-horizontal px-1">
-//             <li className="text-white hover:bg-white tracking-wide text-base">
-//               <a className="hover:bg-transparent font-IBM-Plex-Sans">HOME</a>
-//             </li>
-//             <li className="text-white hover:bg-white font-IBM-Plex-Sans tracking-wide text-base">
-//               <a className="hover:bg-transparent ">RESERVE</a>
-//             </li>
-//             <li class="group relative dropdown px-4 text-white hover:bg-white hover:text-[#707E8B] cursor-pointer font-thin text-base uppercase tracking-wide">
-//               <a className="hover:bg-transparent font-IBM-Plex-Sans text-base">
-//                 PAGES <MdKeyboardArrowDown></MdKeyboardArrowDown>
-//               </a>
-//               <div class="group-hover:block dropdown-menu absolute hidden h-auto">
-//                 <ul class="top-0 w-48 bg-white px-6 py-8">
-//                   <li class="py-1 hover:bg-[#E9F5FF]">
-//                     <a class="block font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
-//                       about
-//                     </a>
-//                   </li>
-//                   <li class="py-1 hover:bg-[#E9F5FF]">
-//                     <a class="block  font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
-//                       menu
-//                     </a>
-//                   </li>
-//                   <li class="py-1 hover:bg-[#E9F5FF]">
-//                     <a class="block  font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
-//                       activities
-//                     </a>
-//                   </li>
-//                   <li class="py-1 hover:bg-[#E9F5FF]">
-//                     <a class="block  font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
-//                       offers
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </div>
-//             </li>
-//             <li className="text-white hover:bg-white text-base">
-//               <a className="hover:bg-transparent font-IBM-Plex-Sans">CONTACT</a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="navbar-end ">
-//           <div className="bg-transparent mr-5 border-[rgb(238,242,255,.3)] border btn-breathe hover:border-[#A7D6FF]">
-//             <button className="btn-sm px-4 bg-[#69727d] m-1.5 rounded-none text-base text-white font-IBM-Plex-Sans tracking-widest hover:bg-[#A7D6FF] hover:text-[#315779] ">
-//               BOOK NOW!
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
-// export default NavBar;
 
 // //TODO: MAKE NAVBAR RESPONSIVE
-
-
 
 const NavBar = () => {
   const [open, setOpen] = useState(false)
@@ -84,11 +21,11 @@ const NavBar = () => {
 
   return (
     <div className="relative flex flex-col w-full h-full">
-      <div className={`bg-red-700 w-[350px] absolute h-screen z-20 lg:hidden ${open? "flex " :"hidden"}`}>
+      <div className={`absolute h-screen z-20 lg:hidden ${open? "flex w-[350px] bg-[#315779] top-0 left-0" :"hidden w-[0]"}`}>
         <NavItems open={open} onClose={handleClose}></NavItems>
       </div>
       <div className="w-full relative flex items-center justify-between bg-[rgb(212,212,216,.1)] h-24 shadow-sm border-b backdrop-blur-md border-[#96989B] border-solid z-10">
-        <div className="w-full lg:flex">
+        <div className={`w-full ${open?'hidden':'lg:flex'}`}>
           <NavItems open={open} onClose={handleClose} ></NavItems>
         </div>
         <div className="flex lg:hidden items-center justify-center h-12 w-12 text-3xl bg-white" onClick={handleClick}>
@@ -100,6 +37,11 @@ const NavBar = () => {
 };
 
 const NavItems = ({ open, onClose }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
   
   return (
     <div className="flex flex-col w-full lg:flex-row lg:justify-between items-center gap-8 ">
@@ -111,26 +53,26 @@ const NavItems = ({ open, onClose }) => {
       </div>
 
       <div className={` lg:flex  w-full  ${open? 'flex' : 'hidden'}`}>
-        <ul className="w-full flex flex-col gap-5 lg:flex-row px-1">
-          <li className="text-white hover:bg-white tracking-wide text-base">
+        <ul className=" w-full flex flex-col gap-5 lg:flex-row px-1">
+          <li className=" flex items-center bg-[#A7D6FF] tracking-wide text-base text-[#315779] px-4 py-3 lg:py-1 cursor-pointer ">
             <a className="hover:bg-transparent font-IBM-Plex-Sans">HOME</a>
           </li>
-          <li className="text-white hover:bg-white font-IBM-Plex-Sans tracking-wide text-base">
+          <li className="flex items-center px-4 py-3 lg:py-1 text-white hover:bg-white font-IBM-Plex-Sans tracking-wide text-base hover:text-[#315779]  ">
             <a className="hover:bg-transparent ">RESERVE</a>
           </li>
-          <li class="group relative dropdown px-4 text-white hover:bg-white hover:text-[#707E8B] cursor-pointer font-thin text-base uppercase tracking-wide">
+          <li onClick={toggleDropdown} class="group relative dropdown px-4 py-3 lg:py-1 text-white hover:bg-white hover:text-[#315779] cursor-pointer font-thin text-base uppercase tracking-wide">
             <a className="hover:bg-transparent font-IBM-Plex-Sans text-base whitespace-nowrap flex items-center text-start ">
               PAGES <MdKeyboardArrowDown></MdKeyboardArrowDown>
             </a>
-            <div class="group-hover:block dropdown-menu absolute hidden h-auto">
-              <ul class="top-0 w-48 bg-white px-6 py-8">
-                <li class="py-1 hover:bg-[#E9F5FF]">
+            <div className={`lg:group-hover:block dropdown-menu absolute ${dropdownOpen ? 'block' : 'hidden'} h-auto`} >
+              <ul class="top-0 w-48 bg-white px-6 py-8 text-start">
+                <li class="py-1 hover:bg-[#E9F5FF] w-full">
                   <a class="block font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
                     about
                   </a>
                 </li>
                 <li class="py-1 hover:bg-[#E9F5FF]">
-                  <a class="block  font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
+                  <a class="block font-thin text-base uppercase text-[#707E8B] hover:bg-transparent font-IBM-Plex-Sans cursor-pointer">
                     menu
                   </a>
                 </li>
@@ -147,7 +89,7 @@ const NavItems = ({ open, onClose }) => {
               </ul>
             </div>
           </li>
-          <li className="text-white hover:bg-white text-base">
+          <li className="flex items-center text-white hover:bg-white text-base hover:text-[#315779] px-4 py-3 lg:py-1">
             <a className="hover:bg-transparent font-IBM-Plex-Sans">CONTACT</a>
           </li>
         </ul>
@@ -160,6 +102,7 @@ const NavItems = ({ open, onClose }) => {
       </div>
     </div>
   );
+
 };
 
 export default NavBar;
